6e5de25b895c7df1bea920cb1b2a5c6f6c67f9b6
Teemu <teemu@Teemus-MacBook.local>
2012-05-26 19:39:03 +0300

    window.location.protocol

--- static/js/results.js
+++ static/js/results.js
@@ -31,7 +31,7 @@
 	  // todo
 	});
 
-	var app_url = 'http://' + window.location.host + '/mobile',
+	var app_url = window.location.protocol + '//' + window.location.host + '/mobile',
 	  enc_url = encodeURI(app_url);
 
 	// set URL for user
